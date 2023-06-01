4806ec0cc5688f52b01c63b5f61fc7f0d6a39d52
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-01 15:34:52 +0100

    back-end running

--- routes/pets.js
+++ routes/pets.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../model/helper");
-const petMustExist =require("../model/guards/petMustExist");
+const petMustExist = require("../model/guards/petMustExist");
 
 
 // Get pet list
